618fc8ccc5fe6f4abfa5426b0d0b5d19aaec3eaa
Bas van der Hoek <bas@hoek.com>
2019-11-30 17:09:29 +0100

    Initial Commit

---
+++ Classroom Document/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Classroom Document
+//
+//  Created by Bas van der Hoek on 30/11/2019.
+//  Copyright © 2019 Zilverline. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Classroom Document/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Classroom Document
+//
+//  Created by Bas van der Hoek on 30/11/2019.
+//  Copyright © 2019 Zilverline. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
